6aba4fd2881e9b9f08f1b10be47990c265b8e3db
Fabian Sødal Dietrichson <fabiansd1402@gmail.com>
2019-08-07 13:37:14 +0200

    update from local ps

--- webpage.py
+++ webpage.py
@@ -1,16 +1,11 @@
 
 # import flask dependencies
-from flask import Flask, request, make_response, jsonify, render_template
-import requests
-import json
-from datetime import datetime
+from flask import Flask
+from flask import render_template
 
 
 from werkzeug.debug import DebuggedApplication
 # from gen_HTML import generate_HTML_report
-
-
-
 
 
 app = Flask(__name__)
@@ -27,6 +22,7 @@ def index():
 @app.route('/fasit', methods=['GET'])
 def report():
     return render_template('fasitpage.html')
+
 
 # run the app
 if __name__ == '__main__':
